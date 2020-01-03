e3b77a57aa4653b353a384a8c42d2fa7c3e13e91
Mat Longinow <mat@webconnex.com>
2020-01-03 03:09:08 -0800

    R3D5: Puppeteer Setup

--- southwest_pilot_app/app.js
+++ southwest_pilot_app/app.js
@@ -4,34 +4,47 @@ const port = 3080;
 const request = require('request');
 const cheerio = require('cheerio');
 const twil = require('./send_message.js');
+const puppeteer = require('puppeteer');
 
 let aboutMe = '';
 
+(async () => {
+    try {
+        const browser = await puppeteer.launch({headless: false});
+        const page = await browser.newPage();
+
+        await page.goto('http://sce.com');
+
+    }
+    catch (err) {
+        console.log(error(err));
+    }
+
+})();
+
+
+
 // This snippet is from the following tutorial: https://www.youtube.com/watch?v=vsmxMLmroyQ&t=176s
 
-
-function scrapeHTML() {
-    return new Promise ((resolve, reject) => {
-        request('http://myprogrammingjourney.com/', (err, res, html) => {
-            if (!err && res.statusCode === 200) {
-                const $ = cheerio.load(html);
-
-                aboutMe = $("#aboutMe > div > div:nth-child(2) > div:nth-child(2) > p:nth-child(1)").html();
-
-                resolve(aboutMe); // Not sure why this has to, in essence, return aboutMe in order for aboutMe to reflect the above change outside the scope of this promise
-            } else {
-                console.log(res.statusCode, err);
-
-                reject();
-            }
-        });
-    });
-}
-
-scrapeHTML()
-    .then((body) => {
-        twil.sendMessage(body);
-    });
+// function scrapeHTML() {
+//     return new Promise ((resolve, reject) => {
+//         request('http://myprogrammingjourney.com/', (err, res, html) => {
+//             if (!err && res.statusCode === 200) {
+//                 const $ = cheerio.load(html);
+//
+//                 aboutMe = $("#aboutMe > div > div:nth-child(2) > div:nth-child(2) > p:nth-child(1)").html();
+//             }
+//
+//             resolve(aboutMe); // Not sure why this has to, in essence, return aboutMe in order for aboutMe to reflect the above change outside the scope of this promise
+//
+//         });
+//     });
+// }
+//
+// scrapeHTML()
+//     .then((body) => {
+//         twil.sendMessage(body);
+//     });
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
